4b5b2355b2925efee4a19b3bbe11b2dd40d71ca2
milan malla <milanmalla2015@gmail.com>
2022-03-21 20:55:53 +0545

    Dashboard added

--- includes/Admin/Admin_Menus.php
+++ includes/Admin/Admin_Menus.php
@@ -26,14 +26,32 @@ class Admin_Menus {
 	 */
 	public function __construct() {
 		// Add menus.
-		add_action( 'admin_menu', array( $this, 'admin_menu' ) );
+		add_action( 'admin_menu', [ $this, 'admin_menu' ] );
+		add_action( 'admin_enqueue_scripts', array( $this, 'init' ) );
 	}
 
+	public function init() {
+		if( 'wpretail' === $_GET['page'] ) {
+			wp_enqueue_style( 'wpretail_style_font', plugins_url( '/assets/fontawesome/css/all.min.css', WPRETAIL_PLUGIN_FILE ) , [] );
+			wp_enqueue_style( 'wpretail_style_bootstrap', plugins_url( '/assets/bootstrap/css/bootstrap.css', WPRETAIL_PLUGIN_FILE ) , [] );
+			wp_enqueue_style( 'wpretail_style_layout', plugins_url( '/assets/css/layout.css', WPRETAIL_PLUGIN_FILE) , [], WPRETAIL_VERSION );
+		}
+	}
 
 	/**
 	 * Add menu items.
 	 */
 	public function admin_menu() {
-		add_menu_page( esc_html__( 'WP Retail', ' wpretail' ), esc_html__( 'WP Retail', 'wpretail' ), 'manage_options', 'wpretail', null,'dashicons-businessman', '25' );
+		add_menu_page( esc_html__( 'WP Retail', ' wpretail' ), esc_html__( 'WP Retail', 'wpretail' ), 'manage_options', 'wpretail', null, 'dashicons-businessman', '25' );
+		add_menu_page( esc_html__( 'Dashboard', ' wpretail' ), esc_html__( 'Dashboard', 'wpretail' ), 'manage_options', 'wpretail', [ $this, 'dashboard' ], 'dashicons-businessman', '25' );
+	}
+
+	/**
+	 * Dashboard
+	 *
+	 * @return void
+	 */
+	public function dashboard() {
+		include plugin_dir_path( WPRETAIL_PLUGIN_FILE ) . '/templates/layout.php';
 	}
 }
