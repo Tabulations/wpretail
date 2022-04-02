32ac90e44cdbe8c84824de3b3ad8ebdb0070585c
milan malla <milanmalla2015@gmail.com>
2022-04-02 12:29:56 +0545

    Update - Dashboard

--- views/dashboard.php
+++ views/dashboard.php
@@ -1,55 +1,34 @@
 <div class="wpretail-main container-fluid">
     <div class="row flex-nowrap">
         <div class="wpretail-sidebar col-auto col-md-3 col-xl-2 px-0">
-            <div class="d-flex flex-column px-0">
-                <a href="/" class="wpretail-sidebar-label -d-flex align-middle">
+            <div class="d-flex flex-column">
+                <!-- <a href="/" class="wpretail-sidebar-label -d-flex align-middle">
                     <span class="fs-5 d-none d-sm-inline">Analytics</span>
-                </a>
+                </a> -->
                 <ul class="nav nav-pills flex-column mb-sm-auto mb-0" id="menu">
                     <li class="nav-item">
-                        <a href="#" class="nav-link wpretail-sidebar-link align-middle">
-                            <i class="fa fa-home"></i> <span class="ms-1 d-none d-sm-inline">Home</span>
+                        <a href="#" class="nav-link wpretail-sidebar-link align-middle active">
+                            <i class="fa-solid fa-bag-shopping"></i> <span class="ms-1 d-none d-sm-inline">Purchase</span>
                         </a>
                     </li>
-                    <li class="nav-item">
-                        <a href="#purchase" data-bs-toggle="collapse" class="nav-link wpretail-sidebar-link align-middle active">
-						<i class="fa fa-bag-shopping"></i></i> <span class="ms-1 d-none d-sm-inline">Purchase</span> </a>
-							<ul class="collapse hide nav flex-column ms-1" id="purchase" data-bs-parent="#menu">
-								<li class="w-100">
-									<a href="#" class="nav-link wpretail-sidebar-link-inner"> <i class="fa-solid fa-caret-right"></i> Today</a>
-								</li>
-								<li>
-									<a href="#" class="nav-link wpretail-sidebar-link-inner"> <i class="fa-solid fa-caret-right"></i> <span class="d-none d-sm-inline">This</span> Week</a>
-								</li>
-								<li>
-									<a href="#" class="nav-link wpretail-sidebar-link-inner"> <i class="fa-solid fa-caret-right"></i> <span class="d-none d-sm-inline">This</span> Month</a>
-								</li>
-								<li>
-									<a href="#" class="nav-link wpretail-sidebar-link-inner"> <i class="fa-solid fa-caret-right"></i> <span class="d-none d-sm-inline">This</span> Year</a>
-								</li>
-							</ul>
+					<li class="nav-item">
+                        <a href="#" class="nav-link wpretail-sidebar-link align-middle">
+                            <i class="fa-solid fa-sack-dollar"></i> <span class="ms-1 d-none d-sm-inline">Sales</span>
+                        </a>
                     </li>
-                    <li class="nav-item">
-						<a href="#sales" data-bs-toggle="collapse" class="nav-link wpretail-sidebar-link align-middle">
-						<i class="fa fa-sack-dollar"></i></i> <span class="ms-1 d-none d-sm-inline">Sales</span> </a>
-						<ul class="collapse hide nav flex-column ms-1" id="sales" data-bs-parent="#menu">
-								<li class="w-100">
-									<a href="#" class="nav-link wpretail-sidebar-link-inner"> <i class="fa-solid fa-caret-right"></i> Today</a>
-								</li>
-								<li>
-									<a href="#" class="nav-link wpretail-sidebar-link-inner"> <i class="fa-solid fa-caret-right"></i> <span class="d-none d-sm-inline">This</span> Week</a>
-								</li>
-								<li>
-									<a href="#" class="nav-link wpretail-sidebar-link-inner"> <i class="fa-solid fa-caret-right"></i> <span class="d-none d-sm-inline">This</span> Month</a>
-								</li>
-								<li>
-									<a href="#" class="nav-link wpretail-sidebar-link-inner"> <i class="fa-solid fa-caret-right"></i> <span class="d-none d-sm-inline">This</span> Year</a>
-								</li>
-							</ul>
+					<li class="nav-item">
+                        <a href="#" class="nav-link wpretail-sidebar-link align-middle">
+                            <i class="fa-solid fa-comments-dollar"></i> <span class="ms-1 d-none d-sm-inline">Tax</span>
+                        </a>
+                    </li>
+					<li class="nav-item">
+                        <a href="#" class="nav-link wpretail-sidebar-link align-middle">
+							<i class="fa-solid fa-receipt"></i> <span class="ms-1 d-none d-sm-inline">Profit</span>
+                        </a>
                     </li>
                 </ul>
                 <hr>
-                <div class="dropdown pb-4">
+                <div class="user dropdown pb-4 pl-4">
                     <a href="#" class="d-flex align-items-center text-dark text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
                         <img src="https://github.com/mdo.png" alt="hugenerd" width="30" height="30" class="rounded-circle">
                         <span class="d-none d-sm-inline mx-1">Milan Malla</span>
@@ -68,7 +47,7 @@
         <div class="col py-3">
             <div class="container">
 				<div class="row">
-					<div class="col-md-3">
+					<div class="col-md-4">
 						<div class="card">
 							<i class="fa-solid fa-bag-shopping"></i>
 							<div class="card-body">
@@ -78,7 +57,7 @@
 							</div>
 						</div>
 					</div>
-					<div class="col-md-3">
+					<div class="col-md-4">
 						<div class="card">
 							<i class="fa-solid fa-bag-shopping"></i>
 							<div class="card-body">
@@ -88,17 +67,7 @@
 							</div>
 						</div>
 					</div>
-					<div class="col-md-3">
-						<div class="card">
-							<i class="fa-solid fa-bag-shopping"></i>
-							<div class="card-body">
-								<h5 class="card-title">Card title</h5>
-								<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-								<a href="#" class="btn btn-primary">Go somewhere</a>
-							</div>
-						</div>
-					</div>
-					<div class="col-md-3">
+					<div class="col-md-4">
 						<div class="card">
 							<i class="fa-solid fa-bag-shopping"></i>
 							<div class="card-body">
